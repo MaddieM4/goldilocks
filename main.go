8e86c08436b066562dc224d7dca72a887755c2ec
Philip Horger <campadrenalin@gmail.com>
2013-09-17 17:35:56 -0700

    Move subcommand code to its own file

--- main.go
+++ main.go
@@ -3,32 +3,6 @@ package main
 import "fmt"
 import "flag"
 
-type GLSubcommand interface {
-    GetName() string
-    GetUsage() string
-    GetDescription() string
-
-    Run(args []string)
-}
-
-func getSubcommands() []GLSubcommand {
-    return []GLSubcommand{
-        Cron{},
-        Tmpl{},
-        Help{},
-    }
-}
-
-func getSubcommandMap() map[string]GLSubcommand {
-    cmnd_list := getSubcommands()
-    cmnd_map := make(map[string]GLSubcommand, len(cmnd_list))
-
-    for _, value := range cmnd_list {
-        cmnd_map[value.GetName()] = value
-    }
-    return cmnd_map
-}
-
 func usage() {
     fmt.Printf("usage: goldilocks COMMAND [ args ]\n\n")
     fmt.Printf("Known subcommands are:\n")
---
+++ subcommand.go
@@ -0,0 +1,27 @@
+package main
+
+type GLSubcommand interface {
+    GetName() string
+    GetUsage() string
+    GetDescription() string
+
+    Run(args []string)
+}
+
+func getSubcommands() []GLSubcommand {
+    return []GLSubcommand{
+        Cron{},
+        Tmpl{},
+        Help{},
+    }
+}
+
+func getSubcommandMap() map[string]GLSubcommand {
+    cmnd_list := getSubcommands()
+    cmnd_map := make(map[string]GLSubcommand, len(cmnd_list))
+
+    for _, value := range cmnd_list {
+        cmnd_map[value.GetName()] = value
+    }
+    return cmnd_map
+}
